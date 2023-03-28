35accf13919c4659364dbcf49bb467748c8e72c0
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2023-03-28 11:19:22 -0300

    Add files via upload

--- Errores-De-Tipeo/src/ErroresDeTipeo.java
+++ Errores-De-Tipeo/src/ErroresDeTipeo.java
@@ -1,3 +1,4 @@
+
 public class ErroresDeTipeo {
 	
 	public static void main(String[] args) {
@@ -7,15 +8,24 @@ public class ErroresDeTipeo {
 	}
 	
 	public int procesador(String texto, String prueba) {
+//		int tamText = texto.length();
 		int cantidadMovimientos = 0;
 		
 		String s = prueba;
 		for (int i = 0; i < texto.length() - 1; i++) {
 			String letra = "" + texto.charAt(i); // obtengo la letra
 			int posEncontrada = s.indexOf(letra);
+			// int postEncontrada=s.indexOf(letra);
 
+			// int posEncontrada=s.indexOf(this.texto.substring(i,i+1)); //busco la posicion
+			// en el string desordenado
 			cantidadMovimientos = cantidadMovimientos + posEncontrada; // seteo los movimientos
+			// s.deleteCharAt(posEncontrada); //elimino el char encontrado
 			s = s.replaceFirst(letra, "");
+			
+			// En la proxima vuelta, al haber eliminado un char, el index sera distinto.
+//			System.out.println(s.length());
+
 		}
 
 		System.out.println(cantidadMovimientos);
